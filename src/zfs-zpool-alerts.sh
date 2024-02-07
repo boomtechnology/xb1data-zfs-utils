db8f2cf614d5a70f0782365eda95a62a25c2493b
Rob Schaefer <rob.schaefer@boom.aero>
2024-02-06 17:01:53 -0800

    put avionics notifications email address in

--- src/zfs-zpool-alerts.sh
+++ src/zfs-zpool-alerts.sh
@@ -6,9 +6,7 @@ ALWAYS=false
 OPTIND=1
 ZSTATUS=$(zpool status)
 ZHEALTHY=$(echo "$ZSTATUS" | grep "state: ONLINE" | wc -l)
-ALERT_EMAIL="rob@boom.aero"
-#ALERT_EMAIL="avionics.notifications@boom.aero"
-NL="%0D%0A"
+ALERT_EMAIL="avionics.notifications@boom.aero"
 
 # Check for the -a option, if present set ALWAYS to true
 while getopts 'a' opt; do
